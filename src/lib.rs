9804311103048d95b58ebeba2f5e6e240f2f0b25
Brendan Molloy <brendan@technocreatives.com>
2019-07-15 17:31:11 +0200

    More prelude

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,10 @@ pub mod exception;
 pub mod vec;
 
 pub mod prelude {
-    pub use crate::macros::*;
     pub use crate::nullable::*;
     pub use crate::sync::*;
     pub use crate::exception::*;
     pub use crate::vec::*;
+    pub use crate::macros::*;
+    pub use crate::{try_as_ref, try_not_null, not_null_or_return};
 }
