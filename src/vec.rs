26198a8a9e3de0485a61475f9a786f094ef345f3
Brendan Molloy <brendan@technocreatives.com>
2019-07-17 19:30:50 +0200

    Move vec ffi into submodule

--- src/vec.rs
+++ src/vec.rs
@@ -14,37 +14,28 @@ use crate::{
     sync::ArcPtr,
 };
 
-#[derive(Debug, Clone)]
-pub struct TypedVoid(TypeId, Arc<dyn Any + 'static + Send + Sync>);
-unsafe impl Send for TypedVoid {}
-unsafe impl Sync for TypedVoid {}
+pub mod ffi;
 
-impl TypedVoid {
-    fn resolve<T: 'static + Send + Sync>(&self) -> Result<Arc<T>, TypedVoid> {
+pub type RawValue = dyn Any + 'static + Send + Sync;
+
+#[derive(Debug, Clone)]
+pub struct TaggedAny(TypeId, Arc<RawValue>);
+unsafe impl Send for TaggedAny {}
+unsafe impl Sync for TaggedAny {}
+
+impl TaggedAny {
+    fn resolve<T: 'static + Send + Sync>(&self) -> Result<Arc<T>, TaggedAny> {
         let x = self.clone();
         match x.1.downcast() {
             Ok(v) => Ok(v),
-            Err(e) => Err(TypedVoid(x.0, e)),
+            Err(e) => Err(TaggedAny(x.0, e)),
         }
     }
 }
 
 #[repr(transparent)]
-#[derive(Debug)]
-pub(crate) struct ThreadSafePtr<T>(*const T);
-unsafe impl<T> Send for ThreadSafePtr<T> {}
-unsafe impl<T> Sync for ThreadSafePtr<T> {}
-impl<T> core::ops::Deref for ThreadSafePtr<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        unsafe { &*self.0 }
-    }
-}
-
-#[repr(transparent)]
 #[derive(Debug, Clone)]
-pub struct AnyVec(Arc<RwLock<RealVec<TypedVoid>>>);
+pub struct AnyVec(Arc<RwLock<RealVec<TaggedAny>>>);
 
 impl AnyVec {
     #[inline]
@@ -61,24 +52,23 @@ impl AnyVec {
     pub fn push<T: Send + Sync + 'static>(&mut self, item: T) {
         self.0
             .write()
-            .push(TypedVoid(TypeId::of::<T>(), Arc::new(item)));
+            .push(TaggedAny(TypeId::of::<T>(), Arc::new(item)));
     }
 
     #[inline]
-    pub fn pop<T: Send + Sync + 'static>(&mut self) -> Option<Result<Arc<T>, TypedVoid>> {
+    pub fn pop<T: Send + Sync + 'static>(&mut self) -> Option<Result<Arc<T>, TaggedAny>> {
         let typed_void = self.0.write().pop()?;
         Some(typed_void.resolve())
     }
 
     #[inline]
-    pub fn get<T: Send + Sync + 'static>(&self, index: usize) -> Option<Result<Arc<T>, TypedVoid>> {
+    pub fn get<T: Send + Sync + 'static>(&self, index: usize) -> Option<Result<Arc<T>, TaggedAny>> {
         let guard = self.0.read();
         let typed_void = guard.get(index)?;
         Some(typed_void.resolve())
     }
 }
 
-type RawValue = dyn Any + 'static + Send + Sync;
 #[derive(Debug, Clone)]
 pub struct RawVec {
     vec: Arc<RwLock<RealVec<Arc<RawValue>>>>,
@@ -214,122 +204,6 @@ impl<T: Send + Sync + 'static + Clone> From<&RealVec<T>> for Vec<T> {
         Vec::from(&**vec)
     }
 }
-
-#[no_mangle]
-pub extern "C" fn vec_len(handle: In<RawVec>, exception: OutPtr<Exception>) -> usize {
-    let handle = try_not_null!(handle.as_ptr(), &exception, 0usize);
-    let handle = unsafe { &*handle.as_ptr() };
-    handle.len()
-}
-
-#[no_mangle]
-pub extern "C" fn vec_push(mut handle: InOut<RawVec>, value: InRaw, exception: OutPtr<Exception>) {
-    let handle = unsafe { try_as_mut_ref!(handle, &exception, ()) };
-    handle.push(Arc::new(value));
-}
-
-macro_rules! vec_nullable {
-    ($thing:expr) => {
-        match $thing {
-            Some(v) => {
-                let arc = $crate::sync::ArcPtr::from(v);
-                $crate::nullable::Nullable::new(arc.as_ptr() as *const _)
-            }
-            None => $crate::nullable::null(),
-        }
-    };
-}
-
-#[no_mangle]
-pub extern "C" fn vec_pop(
-    mut handle: InOut<RawVec>,
-    exception: OutPtr<Exception>,
-) -> Nullable<ArcPtr<RawValue>> {
-    let handle = unsafe { try_as_mut_ref!(handle, &exception) };
-    vec_nullable!(handle.pop())
-    // item)
-
-    // .into().into::<Nullable<_>>()
-}
-
-#[no_mangle]
-pub extern "C" fn vec_get(
-    handle: In<RawVec>,
-    index: u64,
-    exception: OutPtr<Exception>,
-) -> Nullable<ArcPtr<RawValue>> {
-    let handle = unsafe { try_as_ref!(handle, &exception) };
-    vec_nullable!(handle.get(index as usize))
-}
-
-#[macro_export]
-macro_rules! vec_constructor {
-    { $( $ty_name:ident => $ty:ty ),* } => {
-        $(
-            #[no_mangle]
-            pub static $ty_name: TypeId = TypeId::of::<$ty>();
-        )*
-
-        /// A constructor for `Vec` for the C FFI, accepting types provided from generated constants.
-        #[no_mangle]
-        pub extern "C" fn vec_new(ty: TypeId) -> $crate::nullable::Nullable<core::ffi::c_void> {
-            debug!("{:?}", ty);
-            $(
-                if &ty == &$ty_name { return $crate::nullable::Nullable::new(Vec::<$ty>::new().into_raw() as *mut core::ffi::c_void) }
-            )*
-            $crate::nullable::null()
-        }
-
-        /// A function to free vectors.
-        #[no_mangle]
-        pub extern "C" fn vec_free(handle: *mut RawVec, ty: TypeId, exception: *mut Exception) {
-            debug!("{:?}", ty);
-            // TODO: check handle isn't null
-            if handle.is_null() {
-                return;
-            }
-
-            $(
-                if &ty == &$ty_name { unsafe { $crate::vec::Vec::<$ty>::from_raw(handle as *mut _); } }
-            )*
-            // TODO: handle exception
-        }
-
-        #[no_mangle]
-        pub extern "C" fn vec_debug_print(handle: *mut RawVec, ty: TypeId) {
-            debug!("{:?}", ty);
-            // TODO: check handle isn't null
-            if handle.is_null() {
-                return;
-            }
-
-            $(
-                if &ty == &$ty_name {
-                    unsafe {
-                        let raw_vec = &*handle;
-                        let v = raw_vec.iter(|x| x.map(|x| core::mem::transmute::<_, &$ty>(x)).collect::<alloc::vec::Vec<_>>());
-
-                        // let ptr = std::mem::transmute::<&RawVec, &Vec<$ty>>(raw_vec);
-                        debug!("{:?}", &v);
-                    }
-                }
-            )*
-            // TODO: handle exception
-        }
-    };
-}
-
-vec_constructor! {
-    TYPE_STRING => String,
-    TYPE_U64 => u64
-}
-
-// #[no_mangle]
-// pub extern "C" fn string_new(c_str: *const c_char) -> String {
-//     let c_str = unsafe { core::ffi::CStr::from_ptr(c_str) };
-//     debug!("EH: {:?}", &c_str);
-//     c_str.to_str().unwrap().to_string()
-// }
 
 #[cfg(test)]
 mod tests {
---
+++ src/vec/ffi.rs
@@ -0,0 +1,126 @@
+use std::sync::Arc;
+
+use crate::{
+  inout::{In, OutPtr, InOut, InRaw},
+  exception::Exception,
+  vec::RawVec,
+  sync::ArcPtr,
+  nullable::Nullable
+};
+use super::RawValue;
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "no-std")] {
+        use alloc::format;
+    } else {
+        use std::format;
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn vec_len(handle: In<RawVec>, exception: OutPtr<Exception>) -> usize {
+    let handle = try_not_null!(handle.as_ptr(), &exception, 0usize);
+    let handle = unsafe { &*handle.as_ptr() };
+    handle.len()
+}
+
+#[no_mangle]
+pub extern "C" fn vec_push(mut handle: InOut<RawVec>, value: InRaw, exception: OutPtr<Exception>) {
+    let handle = unsafe { try_as_mut_ref!(handle, &exception, ()) };
+    handle.push(Arc::new(value));
+}
+
+macro_rules! vec_nullable {
+    ($thing:expr) => {
+        match $thing {
+            Some(v) => {
+                let arc = $crate::sync::ArcPtr::from(v);
+                $crate::nullable::Nullable::new(arc.as_ptr() as *const _)
+            }
+            None => $crate::nullable::null(),
+        }
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn vec_pop(
+    mut handle: InOut<RawVec>,
+    exception: OutPtr<Exception>,
+) -> Nullable<ArcPtr<RawValue>> {
+    let handle = unsafe { try_as_mut_ref!(handle, &exception) };
+    vec_nullable!(handle.pop())
+}
+
+#[no_mangle]
+pub extern "C" fn vec_get(
+    handle: In<RawVec>,
+    index: u64,
+    exception: OutPtr<Exception>,
+) -> Nullable<ArcPtr<RawValue>> {
+    let handle = unsafe { try_as_ref!(handle, &exception) };
+    vec_nullable!(handle.get(index as usize))
+}
+
+#[macro_export]
+macro_rules! generate_vec_ffi {
+    { $( $ty_name:ident => $ty:ty ),* } => {
+        $(
+            #[no_mangle]
+            pub static $ty_name: core::any::TypeId = core::any::TypeId::of::<$ty>();
+        )*
+
+        /// A constructor for `Vec` for the C FFI, accepting types provided from generated constants.
+        #[no_mangle]
+        pub extern "C" fn vec_new(ty: core::any::TypeId) -> $crate::nullable::Nullable<core::ffi::c_void> {
+            log::debug!("{:?}", ty);
+            $(
+                if &ty == &$ty_name { return $crate::nullable::Nullable::new($crate::vec::Vec::<$ty>::new().into_raw() as *mut core::ffi::c_void) }
+            )*
+            $crate::nullable::null()
+        }
+
+        /// A function to free vectors.
+        #[no_mangle]
+        pub extern "C" fn vec_free(handle: *mut RawVec, ty: core::any::TypeId, exception: *mut Exception) {
+            log::debug!("{:?}", ty);
+            // TODO: check handle isn't null
+            if handle.is_null() {
+                return;
+            }
+
+            $(
+                if &ty == &$ty_name { unsafe { $crate::vec::Vec::<$ty>::from_raw(handle as *mut _); } }
+            )*
+            // TODO: handle exception
+        }
+
+        #[no_mangle]
+        pub extern "C" fn vec_debug_print(handle: *mut RawVec, ty: core::any::TypeId) {
+            log::debug!("{:?}", ty);
+            // TODO: check handle isn't null
+            if handle.is_null() {
+                return;
+            }
+
+            $(
+                if &ty == &$ty_name {
+                    unsafe {
+                        let raw_vec = &*handle;
+                        let v = raw_vec.iter(|x| x.map(|x| core::mem::transmute::<_, &$ty>(x)).collect::<alloc::vec::Vec<_>>());
+                        log::debug!("{:?}", &v);
+                    }
+                }
+            )*
+            // TODO: handle exception
+        }
+    };
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "demo")] {
+        generate_vec_ffi! {
+            TYPE_U64 => u64,
+            TYPE_STRING => std::string::String
+        }
+    }
+}
